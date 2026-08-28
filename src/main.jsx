ca64014afb452d995b5d0320822083bed894f0fc
bongkochkru <bongkochkru@allnow.co.th>
2026-08-28 16:31:10 +0700

    Fix stay vote page startup

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { renderToStaticMarkup } from "react-dom/server";
 import { divIcon } from "leaflet";
